d485f0a6b3a25fe9b299b85a085ec6ace8efdbd7
上田貴央 <gibson511511@gmail.com>
2022-10-09 12:28:56 +0900

    [Ref]sanitizer.ts fix

--- src/libs/sanitizer.ts
+++ src/libs/sanitizer.ts
@@ -1,4 +1,4 @@
-import DOMPurify, { Config } from 'dompurify';
+import DOMPurify from 'dompurify';
 import { marked } from 'marked';
 
 // dangerouslySetInnerHTMLに代入する変数の型.
@@ -8,7 +8,7 @@ type InnerHTML = {
 
 // 出力されるanchorタグにtarget属性とrel属性を付与.
 const renderer = new marked.Renderer();
-renderer.link = (href, title, text) => {
+renderer.link = (href, _, text) => {
 	return `<a rel="noopener" target="_blank" href="${href}">${text}</a>`;
 };
 
@@ -20,17 +20,6 @@ export const convertMarkdownToHTML = (markdownText: string): InnerHTML => {
 		renderer,
 	});
 
-	console.log('markedText', markedText);
-
-	// DOMPurifyでサニタイズする時のオプションを定義.
-	const config: Config = {
-		// 許可する属性.
-		ALLOWED_ATTR: ['href', 'target', 'rel'],
-		// 許可するタグ.
-		ALLOWED_TAGS: ['p', 'br', 'ul', 'ol', 'li', 'blockquote', 'strong', 'em', 'a', 'hr', 'del', 'pre', 'code'],
-	};
-
-	// dangerouslySetInnerHTMLだとstringしか受け付けていないためstring型をアサーション.
 	const htmlText = DOMPurify.sanitize(markedText, { USE_PROFILES: { html: true } });
 	console.log('htmlText', htmlText);
 	return { __html: htmlText };
