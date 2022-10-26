9f56fcaf9d8a83d912a29331b73ec48866792322
上田貴央 <gibson511511@gmail.com>
2022-10-27 01:25:57 +0900

    Changed Theme Color to 'light'

--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-	initialColorMode: 'system',
+	initialColorMode: 'light',
 	useSystemColorMode: false,
 };
 
