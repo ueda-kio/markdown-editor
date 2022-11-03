2922c91d1128bb5356457aace8f32c49b4467d7c
上田貴央 <gibson511511@gmail.com>
2022-11-03 11:18:52 +0900

    [WIP]Fixed Setting Page

--- src/routes/Setting.tsx
+++ src/routes/Setting.tsx
@@ -1,23 +1,72 @@
 import React from 'react';
-import { Box, FormControl, FormLabel, HStack, Radio, RadioGroup, useColorMode } from '@chakra-ui/react';
+import {
+	Box,
+	FormControl,
+	FormLabel,
+	HStack,
+	IconButton,
+	Radio,
+	RadioGroup,
+	Stack,
+	StackDivider,
+	Switch,
+	useColorMode,
+	VStack,
+} from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+import { useNavigate } from 'react-router-dom';
+import { nanoid } from '@reduxjs/toolkit';
 
 const Setting = () => {
+	const navigate = useNavigate();
 	const { colorMode, setColorMode } = useColorMode();
 	const handleChangeTheme = (e: string) => {
 		setColorMode(e);
 	};
+	const switchArray = [
+		{ label: '自動並び替え', id: nanoid(), handleChange: () => console.log('test') },
+		{ label: 'ショートカットで保存', id: nanoid(), handleChange: () => console.log('test') },
+	];
 	return (
 		<Box maxWidth="max" m="0 auto">
-			<FormControl as="fieldset">
-				<FormLabel as="legend">theme</FormLabel>
-				<RadioGroup defaultValue={colorMode} onChange={handleChangeTheme}>
-					<HStack>
-						<Radio value="light">light</Radio>
-						<Radio value="dark">dark</Radio>
-						<Radio value="system">system</Radio>
-					</HStack>
-				</RadioGroup>
-			</FormControl>
+			<IconButton
+				aria-label="open new editor"
+				icon={<ChevronLeftIcon w={6} h={6} />}
+				colorScheme="teal"
+				rounded="full"
+				w="12"
+				h="12"
+				onClick={() => navigate(-1)}
+			></IconButton>
+			<VStack divider={<StackDivider h="1px" bg="gray.200" />} spacing="4" align="stretch">
+				<FormControl as="fieldset">
+					<FormLabel>theme</FormLabel>
+					<RadioGroup defaultValue={colorMode} onChange={handleChangeTheme}>
+						<HStack>
+							<Radio value="light">light</Radio>
+							<Radio value="dark">dark</Radio>
+							<Radio value="system">system</Radio>
+						</HStack>
+					</RadioGroup>
+				</FormControl>
+				<FormControl as="fieldset" display={'flex'}>
+					<FormLabel>style</FormLabel>
+					<RadioGroup defaultValue={colorMode} onChange={handleChangeTheme}>
+						<VStack>
+							<Radio value="light">list</Radio>
+							<Radio value="dark">panel</Radio>
+						</VStack>
+					</RadioGroup>
+				</FormControl>
+				{switchArray.map((elm) => (
+					<FormControl display="flex" alignItems="center" justifyContent={'space-between'}>
+						<FormLabel htmlFor={elm.id} mb="0" flexGrow={'1'} cursor="pointer">
+							{elm.label}
+						</FormLabel>
+						<Switch id={elm.id} onChange={elm.handleChange} />
+					</FormControl>
+				))}
+			</VStack>
 		</Box>
 	);
 };
