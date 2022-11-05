96e32312212640f382e11f75eda443107a3dde12
上田貴央 <gibson511511@gmail.com>
2022-11-06 01:30:00 +0900

    Changed SearchInput Component placeholder style

--- src/components/Molecules/SearchInput.tsx
+++ src/components/Molecules/SearchInput.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { SearchIcon } from '@chakra-ui/icons';
-import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Input, InputGroup, InputLeftElement, useBreakpointValue } from '@chakra-ui/react';
 
 const SearchInput = () => {
+	const placeholderFontSize = useBreakpointValue({ base: '14px', md: '16px' });
 	return (
 		<InputGroup>
 			<InputLeftElement
@@ -12,7 +13,15 @@ const SearchInput = () => {
 				pointerEvents="none"
 				children={<SearchIcon w={{ base: '4', md: '5' }} h={{ base: '4', md: '5' }} color="gray.400" />}
 			/>
-			<Input placeholder="search" size="lg" h={{ base: '10', md: '12' }} rounded="3xl" variant="filled" />
+			<Input
+				placeholder="Search"
+				size="lg"
+				h={{ base: '10', md: '12' }}
+				rounded="3xl"
+				fontSize={{ base: '14px', md: '16px' }}
+				variant="filled"
+				_placeholder={{ fontSize: placeholderFontSize }}
+			/>
 		</InputGroup>
 	);
 };
