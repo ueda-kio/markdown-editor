50acd43d5fa87b9c20980d4a76556891b2a12340
上田貴央 <gibson511511@gmail.com>
2022-11-06 16:31:50 +0900

    Loading Component to full screen

--- src/components/Atoms/Loading.tsx
+++ src/components/Atoms/Loading.tsx
@@ -1,5 +1,9 @@
-import { Spinner } from '@chakra-ui/react';
+import { Flex, Spinner } from '@chakra-ui/react';
 
-const Loading = () => <Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />;
+const Loading = () => (
+	<Flex w="100vw" h="100vh" align={'center'} justify="center">
+		<Spinner thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" size="xl" />
+	</Flex>
+);
 
 export default Loading;
